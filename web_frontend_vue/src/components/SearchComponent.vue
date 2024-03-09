d9a6518c56a92c76e356da7c27b87a8687d25626
sheny <1250873589@qq.com>
2024-03-09 02:13:50 +0000

    Update vue

--- web_frontend_vue/src/components/SearchComponent.vue
+++ web_frontend_vue/src/components/SearchComponent.vue
@@ -1,51 +1,61 @@
 <template>
   <div class="search-container">
-    <input v-model="searchQuery" @keyup.enter="search" placeholder="Enter the search query..." class="search-input" />
-    <button @click="search" class="search-button">Search</button>
+    <!-- <h1 class="search-h1" v-show="!searched">APWikipedia</h1> -->
+    <h1 class="search-h1">APWikipedia</h1>
+    <!-- <div :class="['search-box', searched ? 'top-left-search-box' : 'centered-search-box']"> -->
+      <!-- <h3 class="search-h3" v-show="searched">APWikipedia</h3> -->
+      <input v-model="searchQuery" @keyup.enter="search" placeholder="Enter the search query..." class="search-input" />
+      <button @click="search" class="search-button">Search</button>
+    <!-- </div> -->
     <div v-if="searchResults.length > 0" class="results-container">
       <div v-for="result in searchResults" :key="result.url" class="search-result">
-        <!-- <h3 class="result-title">{{ result.title }}</h3>
-        <a :href="result.url" target="_blank" class="result-link">阅读更多</a> -->
         <h3><a :href="result.url" target="_blank" class="result-link">{{ result.title }}</a></h3>
         <p class="result-summary">{{ result.summary }}</p>
       </div>
       <div v-if="searchTime !== null" class="search-time">Search Time: {{ searchTime }} ms</div>
     </div>
+    <div v-if="searchResults.length === 0 && searched" class="no-results">No results found.</div>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: [],
+      searchTime: null,
+      searched: false, // 用于跟踪是否已经进行了搜索
+    };
+  },
+  methods: {
+    async search() {
+      this.searched = true; // 用户点击搜索，更新状态
+      if (this.searchQuery.trim() === '') {
+        alert('The search query cannot be empty');
+        return;
+      }
 
-const searchQuery = ref('');
-const searchResults = ref([]);
-const searchTime = ref(null);
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: this.searchQuery }),
+      };
 
-const search = async () => {
-  if (searchQuery.value.trim() === '') {
-    alert('The search query cannot be empty');
-    return;
-  }
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+      try {
+        const response = await fetch('http://127.0.0.1:5000/ranked_search', requestOptions);
+        const data = await response.json();
+        this.searchResults = data.results || [];
+        this.searchTime = data['search_time(Ms)'] || null;
+      } catch (error) {
+        console.error("Request Error:", error);
+        this.searchResults = [];
+      }
     },
-    body: JSON.stringify({ query: searchQuery.value })
-  };
-
-  try {
-    const response = await fetch('http://127.0.0.1:5000/ranked_search', requestOptions);
-    const data = await response.json();
-    searchResults.value = data.results || [];
-    searchTime.value = data['search_time(Ms)'] || null;
-  } catch (error) {
-    console.error("Request Error:", error);
-    searchResults.value = [];
-  }
+  },
 };
-
 </script>
 
 <style>
@@ -53,6 +63,47 @@ const search = async () => {
   text-align: center;
   margin-top: 20px;
 }
+
+.search-h1 {
+  font-family: Georgia, serif;
+  font-size: 50px;
+  color: transparent;
+  background: rgb(7, 5, 26);
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+  text-shadow: 0px 3px 3px rgba(255, 255, 255, 0.5);
+}
+
+/* .search-h3 {
+  font-family: Georgia, serif;
+  font-size: 20px;
+  color: transparent;
+  background: rgb(7, 5, 26);
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+  text-shadow: 0px 3px 3px rgba(255, 255, 255, 0.5);
+  margin-right: 10px;
+} */
+
+/* .centered-search-box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+}
+
+.top-left-search-box {
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  transform: translate(-10%, -10%);
+  display: flex;
+  align-items: center;
+} */
 
 .search-input {
   width: 300px;
@@ -78,10 +129,11 @@ const search = async () => {
 .results-container {
   text-align: left;
   margin-top: 20px;
+  /* margin-top: 20vh; */
 }
 
 .search-result {
-  border-bottom: 1px solid #eee;
+  /* border-bottom: 1px solid #eee; */
   padding: 10px 0;
 }
 
@@ -103,5 +155,9 @@ const search = async () => {
 .search-time {
   margin-top: 10px;
   margin-bottom: 10px;
+}
+
+.no-results {
+  margin-top: 20px;
 }
 </style>
--- web_frontend_vue/vue.md
+++ web_frontend_vue/vue.md
@@ -4,6 +4,12 @@
 3. npm install
 4. npm run serve
 
+# TODO
+* 调整搜索前后搜索框位置，替代原来的页面跳转
+* 分页
+* 搜索框优化高级搜索选项
+* spell check和搜索扩展
+
 # Others
 ## Project setup
 ```
