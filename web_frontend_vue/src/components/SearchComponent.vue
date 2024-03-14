6486a7e9f1da1ecfe7a89752cbcdb39d0a47e42a
yongtenglei <yongtengrey@outlook.com>
2024-03-14 23:17:40 +0000

    Change localhost to server IP

--- web_frontend_vue/src/components/SearchComponent.vue
+++ web_frontend_vue/src/components/SearchComponent.vue
@@ -240,7 +240,7 @@ export default {
         }),
       };
       try {
-        const response = await fetch(`http://127.0.0.1:5000/spell_check`, requestOptions);
+        const response = await fetch(`http://34.142.98.9:5000/spell_check`, requestOptions);
         if (response.ok) {
           const result = await response.json();
           this.spellCheckedQuery = result.spell_checked_query;
@@ -262,7 +262,7 @@ export default {
         }),
       };
       try {
-        const response = await fetch(`http://127.0.0.1:5000/word_expansion`, requestOptions);
+        const response = await fetch(`http://34.142.98.9:5000/word_expansion`, requestOptions);
         if (response.ok) {
           const result2 = await response.json();
           this.autocompleteResults = result2.expanded_words;
--- web_frontend_vue/src/views/ResultPage.vue
+++ web_frontend_vue/src/views/ResultPage.vue
@@ -48,7 +48,7 @@ export default {
     },
     computed: {
         searchURL() {
-            return this.isAdvancedSearchActive ? 'http://127.0.0.1:5000/search' : 'http://127.0.0.1:5000/ranked_search';
+            return this.isAdvancedSearchActive ? 'http://34.142.98.9:5000/search' : 'http://34.142.98.9:5000/ranked_search';
         },
     },
     watch: {
